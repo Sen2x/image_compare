db5846b60885f352d969a1f9fdbe141438113c79
Arsenij <arsenijsenin10@gmail.com>
2026-07-20 17:13:33 +0300

    Added image loading and array properties

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 path1= "data_in/image1.png"
 path2= "data_in/image2.png"
@@ -9,9 +10,19 @@ if img1 is None :
     raise FileNotFoundError(f" Image was not found {path1}")
 if img2 is None :
     raise FileNotFoundError(f" Image was not found {path2}")
+print(img1.shape)
+print(img2.shape)
 
-print("First picture loaded ")
-print("First picture shape:", img1.shape)
+print(img1.ndim)
+print(img2.ndim)
 
-print("Second picture loaded ")
-print("Second picture shape:", img2.shape)
+print(img1.dtype)
+print(img2.dtype)
+print(type(img1))
+print(type(img2))
+# print("First picture loaded ")
+# print("First picture shape:", img1.shape)
+
+# print("Second picture loaded ")
+# print("Second picture shape:", img2.shape)
+
